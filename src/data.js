38933287bc5b3ac9a0d80ac9c4e5080f5e804bf4
Shravan Patel <patelshravan24@gmail.com>
2021-07-12 10:28:13 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    title: "INSTAGRAM-CLONE",
+    subtitle: "MERN Stack",
+    description:
+      "This is an Instagram clone, a bit like the opposite of Instagram. Here you can create a profile, upload photos, write, comment, click on others' posts, and much more.",
+    image: "./insta.png",
+    link: "https://instaclone52.herokuapp.com/",
+  },
+  {
+    title: "LIC",
+    subtitle: "React and Python",
+    description:
+      "This is a LIC Agent Portal App in which you can log in and see the details of your clients and much more.",
+    image: "./LIC.png",
+    link: "https://www.linkedin.com/in/patel-shravan/",
+  },
+  {
+    title: "React Website",
+    subtitle: "React and CSS",
+    description:
+      "This is just a webpage that I made using React JS and CSS. Which represents a travel website.",
+    image: "./trvl.png",
+    link: "https://www.instagram.com/_meshravanpatel/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Hi, I'm Ganesh, and I'd like to welcome you to my world. Quill Bot employs me as a software engineer. Shravan was given a few projects. He did a fantastic job on those assignments, and he did it on time. He makes good use of his time and completes assignments on schedule.",
+    image: "ganesh1.jpg",
+    name: "Ganesh Patel",
+    company: "Quill Bot",
+  },
+  {
+    quote:
+      "Hello, my name is Kailash. I'm currently employed as a developer at DOODLE. Shravan is a talented web designer. He has completed numerous jobs, and I have also assigned him projects. He completed the projects in a timely and professional manner.",
+    image: "kp.jpg",
+    name: "Kailash Patel",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Material UI",
+  "HTML",
+  "MONGO DB",
+  "Python"
+];
